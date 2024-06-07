e2403858818e716dc1bd5140e4544f47ded38701
oscarhibbert <oscarhibbert1@gmail.com>
2024-06-07 18:10:34 +0100

    Main setup in progress

---
+++ election-predictor/interface/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+def predict_election(election_year:int):
+    """
+    Predicts the outcome of the specified UK general election.
+    """
