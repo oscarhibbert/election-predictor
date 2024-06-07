7abdcccfc834d9ec99c0812122b5c02981415c63
oscarhibbert <oscarhibbert1@gmail.com>
2024-06-07 19:52:23 +0100

    Planning inside main.py

--- election-predictor/interface/main.py
+++ election-predictor/interface/main.py
@@ -1,8 +1,28 @@
 import numpy as np
 import pandas as pd
 from datetime import datetime
+# Import all functions inside data.py from ml_logic
 
-def predict_election(election_year:int):
+
+
+def predict_election(election_year:int) -> dict:
     """
     Predicts the outcome of the specified UK general election.
+
+    :param election_year: The election cycle year to predict.
+    :return: A dictionary containing the predicted election results.
     """
+
+    #TODO Handle election cycle date logic
+
+    #TODO Handle data source fetching
+
+    #TODO Handle data source cleaning
+
+    #TODO Handle preprocessing
+
+    #TODO Handle modelling
+
+    #TODO Handle model evaluation
+
+    #TODO Handle prediction
